a981519974d232012739f48ac4046a9981c8987d
cristobal2323 <cri.maturana@gmail.com>
2020-01-23 20:22:07 +0000

    Controles del reproductor

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,18 +1,54 @@
 import React, {Component} from 'react';
 import Video from 'react-native-video';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ActivityIndicator, Text} from 'react-native';
 import Layout from '../components/layout';
+import ControlLayout from '../components/control-layout';
+import PlayPause from '../components/play-pause';
 
 class Player extends Component {
+  state = {
+    loading: true,
+    paused: false,
+  };
+  onBuffer = ({isBuffering}) => {
+    this.setState({
+      loading: isBuffering,
+    });
+  };
+  onLoad = () => {
+    this.setState({
+      loading: false,
+    });
+  };
+  playPause = () => {
+    this.setState((oldState, oldProps) => {
+      return {
+        paused: !oldState.paused,
+      };
+    });
+  };
   render() {
     return (
       <Layout
+        loading={this.state.loading}
         video={
           <Video
             source={require('./../../../assets/video.mp4')}
             style={styles.video}
             resizeMode="contain"
+            onBuffer={this.onBuffer}
+            onLoad={this.onLoad}
+            paused={this.state.paused}
           />
+        }
+        loader={<ActivityIndicator color="red" />}
+        controls={
+          <ControlLayout>
+            <PlayPause onPress={this.playPause} paused={this.state.paused} />
+            <Text>progress bar | </Text>
+            <Text>time left | </Text>
+            <Text>fullscreen | </Text>
+          </ControlLayout>
         }
       />
     );
