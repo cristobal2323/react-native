0554b322951408834d4aea4d5614681ab0aa3a88
cristobal2323 <cri.maturana@gmail.com>
2020-01-26 18:28:35 +0000

    Organizando componentes

--- App.js
+++ App.js
@@ -1,49 +1,18 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './store';
 
-import Home from './src/screens/containers/home';
-import Header from './src/sections/components/header';
-import SuggestionList from './src/videos/containers/suggestion-list';
-import CategoryList from './src/videos/containers/category-list.js';
-import Player from './src/player/containers/player';
-
-import API from './utils/api';
+import Loading from './src/sections/components/loading';
+import AppLayout from './src/app';
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = {};
-
-  async componentDidMount() {
-    const categoryList = await API.getMovies();
-    store.dispatch({
-      type: 'SET_CATEGORY_LIST',
-      payload: {
-        categoryList,
-      },
-    });
-    const suggestionList = await API.getSuggestion(10);
-    store.dispatch({
-      type: 'SET_SEGGESTION_LIST',
-      payload: {
-        suggestionList,
-      },
-    });
-  }
-
   render() {
     return (
       <Provider store={store}>
-        <PersistGate loading={<Text>cargando...</Text>} persistor={persistor}>
-          <Home>
-            <Header />
-            <Player />
-            <Text>buscador</Text>
-            <CategoryList />
-            <SuggestionList />
-          </Home>
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <AppLayout />
         </PersistGate>
       </Provider>
     );
