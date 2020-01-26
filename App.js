f4e935378f6a17ae357b112310888d6618f99e25
cristobal2323 <cri.maturana@gmail.com>
2020-01-26 17:21:50 +0000

    Crear los reducers

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import store from './store';
 
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
@@ -11,28 +13,37 @@ import API from './utils/api';
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = {
-    suggestionList: [],
-    categoryList: [],
-  };
+  state = {};
+
   async componentDidMount() {
-    const movies = await API.getSuggestion(10);
-    const categories = await API.getMovies();
-    this.setState({
-      suggestionList: movies,
-      categoryList: categories,
+    const categoryList = await API.getMovies();
+    store.dispatch({
+      type: 'SET_CATEGORY_LIST',
+      payload: {
+        categoryList,
+      },
+    });
+    const suggestionList = await API.getSuggestion(10);
+    store.dispatch({
+      type: 'SET_SEGGESTION_LIST',
+      payload: {
+        suggestionList,
+      },
     });
   }
+
   render() {
     return (
-      <Home>
-        <Header />
-        <Player />
-        <Text>buscador</Text>
-        <Text>categorías</Text>
-        <CategoryList list={this.state.categoryList} />
-        <SuggestionList list={this.state.suggestionList} />
-      </Home>
+      <Provider store={store}>
+        <Home>
+          <Header />
+          <Player />
+          <Text>buscador</Text>
+          <Text>categorías</Text>
+          <CategoryList />
+          <SuggestionList />
+        </Home>
+      </Provider>
     );
   }
 }
